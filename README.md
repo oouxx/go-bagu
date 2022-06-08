c33486e1246dad34a68e6b9499002caa7fe76e83
wangxinxing <wangxinxing@zeehug.com>
2022-06-08 16:18:44 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # go-bagu
+
+## go基础
+
+### go Map
+[三种线程安全的Map](https://zhuanlan.zhihu.com/p/356739568)
+
+### sync.Pool
+[sync.Pool使用场景](https://geektutu.com/post/hpg-sync-pool.html)
+### golang 文件读取效率
+[Go语言三种方式读取文件效率对比及原因分析](https://blog.csdn.net/C_J33/article/details/82957394)
+
+### golang 字符串拼接
+[字符串拼接性能及原理](https://geektutu.com/post/hpg-string-concat.html)
+## others
+[面试必备（背）](https://xie.infoq.cn/article/ac87ac5f9e8def9f91b817bf9)
---
+++ stringconcat/stringconcat_test.go
@@ -0,0 +1,35 @@
+package stringconcat
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func benchmark(b *testing.B, f func(int, string) string) {
+	var str = randomString(10)
+	for i := 0; i < b.N; i++ {
+		f(10000, str)
+	}
+}
+
+func BenchmarkPlusConcat(b *testing.B)    { benchmark(b, plusConcat) }
+func BenchmarkSprintfConcat(b *testing.B) { benchmark(b, sprintfConcat) }
+func BenchmarkBuilderConcat(b *testing.B) { benchmark(b, builderConcat) }
+func BenchmarkBufferConcat(b *testing.B)  { benchmark(b, bufferConcat) }
+func BenchmarkByteConcat(b *testing.B)    { benchmark(b, byteConcat) }
+func BenchmarkPreByteConcat(b *testing.B) { benchmark(b, preByteConcat) }
+
+
+func TestBuilderConcat(t *testing.T) {
+	var str = randomString(10)
+	var builder strings.Builder
+	cap := 0
+	for i := 0; i < 10000; i++ {
+		if builder.Cap() != cap {
+			fmt.Print(builder.Cap(), " ")
+			cap = builder.Cap()
+		}
+		builder.WriteString(str)
+	}
+}
